72ca52b32c2ca62d01329de1fa31cde61fac0c39
Wei Yuxin <weiyuxin100@gmail.com>
2020-08-31 11:33:42 +0800

    updated index, script and styles

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hi');
+
+var i = 0;
+var delay = 50;
+var text = "Programmer";
+
+
+function identity(){
+  if (i < text.length){
+    document.getElementById("animation").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(identity, delay);
+  }
+}
